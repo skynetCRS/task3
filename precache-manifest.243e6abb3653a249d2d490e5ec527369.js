2388a4608631a4f01a789e1b4cbb29eb09425bae
skynetCRS <ashoktammisetti00@gmail.com>
2020-10-06 18:22:13 +0530

    Updates

---
+++ precache-manifest.243e6abb3653a249d2d490e5ec527369.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0710b866abeeb0b988cc1cd2a16b24b8",
+    "url": "/task3/index.html"
+  },
+  {
+    "revision": "2095c5c08f647ebd69bd",
+    "url": "/task3/static/css/main.e82f1e61.chunk.css"
+  },
+  {
+    "revision": "98333de722f4ff9fb751",
+    "url": "/task3/static/js/2.3ba88831.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/task3/static/js/2.3ba88831.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2095c5c08f647ebd69bd",
+    "url": "/task3/static/js/main.ca106784.chunk.js"
+  },
+  {
+    "revision": "b646cc790acdedea52dc",
+    "url": "/task3/static/js/runtime-main.bd19fd05.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/task3/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "3d1df7b48a25fe9426e30094d80f9d00",
+    "url": "/task3/static/media/resume.3d1df7b4.jpg"
+  }
+]);
